4dfa8374c0920ae7c7f57dcb10c875fda9ef52c7
Joydip Dey <joydipd@nihilentanalytics.com>
2019-11-29 11:53:19 +0530

    first-commit

--- actions/wish.py
+++ actions/wish.py
@@ -1,9 +1,11 @@
 import sys
+import datetime
 from st2common.runners.base_action import Action
 from u_procs import part_of_a_day
 
 class MyAction(Action):
 
-    def run(self, person, ctim):
-        #print('Hi ' + person + ' Good ' + part_of_a_day(ctim))
-        return('Hi ' + person + ' Good ' + part_of_a_day(ctim))
+    def run(self, person):
+        timenow = datetime.datetime.now()
+        hrofdy = timenow.hour
+        return('Hi ' + person + ' Good ' + part_of_a_day(hrofdy))
